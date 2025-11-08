024c52fc6645c72f645ec2b08812e2bed4010c96
MEV1N <mevinmanu04@gmail.com>
2025-11-09 01:52:40 +0530

    test

--- detect.py
+++ detect.py
@@ -5,9 +5,5 @@ mode = "predict"
 model = yolo("yolo11n.pt")
 source = "2.webp"
 
-# Run prediction and save results
-results = model(source, save=True)
 
-# Alternatively, you can also access the results and save manually:
-# for r in results:
-#     r.save(filename='result.jpg')  # Save to a specific file
+results = model(source, save=True)
